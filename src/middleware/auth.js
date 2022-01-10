a64d8f0c07c2848aaa3c7274a72eceaa95a88975
YuTingtao <735126858@qq.com>
2022-01-10 20:20:38 +0800

    update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,3 +1,4 @@
+// auth中间件demo
 export default function({ store, redirect }) {
     if (!store.state.token) {
         return redirect('/');
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+// vuex
 export const state = ()=> ({
     token: '',
     userInfo: {}
